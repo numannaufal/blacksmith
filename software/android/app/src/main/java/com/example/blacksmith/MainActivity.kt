e57ecde3bd1611ff90081913c34049712f39d681
Nu'man Naufal <numan.naufal@happyfresh.com>
2022-08-06 12:00:15 +0700

    Software > Android > Second Activity

--- software/android/app/src/main/java/com/example/blacksmith/MainActivity.kt
+++ software/android/app/src/main/java/com/example/blacksmith/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.blacksmith
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var textViewTitle: TextView
     private lateinit var imageViewMascot: ImageView
+    private lateinit var buttonUpgrade: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +22,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         buildTitle()
         buildImage()
+        buildButton()
     }
 
     private fun buildTitle() {
@@ -29,5 +33,14 @@ class MainActivity : AppCompatActivity() {
     private fun buildImage() {
         imageViewMascot = findViewById(R.id.mascot)
         imageViewMascot.visibility = View.VISIBLE
+    }
+
+    private fun buildButton() {
+        buttonUpgrade = findViewById(R.id.button)
+        buttonUpgrade.setText(R.string.upgrade)
+        buttonUpgrade.setOnClickListener {
+            var intent = Intent(this, ToolBoxActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
