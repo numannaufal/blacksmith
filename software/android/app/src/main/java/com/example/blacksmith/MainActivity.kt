2bf1ea2a3f43ffb52ced8bcbffe6245f130cfa4b
Nu'man Naufal <numan.naufal@happyfresh.com>
2022-08-06 10:22:28 +0700

    Software > Android > Views: Centering UI

--- software/android/app/src/main/java/com/example/blacksmith/MainActivity.kt
+++ software/android/app/src/main/java/com/example/blacksmith/MainActivity.kt
@@ -6,14 +6,12 @@ import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var textView: TextView
-    private lateinit var mascot: ImageView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        buildViews();
+    }
 
-        textView = findViewById(R.id.textView)
-        textView.setText(R.string.app_name)
+    private fun buildViews() {
+        setContentView(R.layout.activity_main)
     }
 }
