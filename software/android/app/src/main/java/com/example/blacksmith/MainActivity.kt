07a60988af87780b43c2c13519c6c01a24dba258
Nu'man Naufal <numan.naufal@happyfresh.com>
2022-08-06 10:58:40 +0700

    Software > Android > Views: Showing image

--- software/android/app/src/main/java/com/example/blacksmith/MainActivity.kt
+++ software/android/app/src/main/java/com/example/blacksmith/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.blacksmith
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var textViewTitle: TextView
+    private lateinit var imageViewMascot: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +22,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun buildTitle() {
-        textViewTitle = findViewById(R.id.textView)
+        textViewTitle = findViewById(R.id.title)
         textViewTitle.setText(R.string.app_name)
     }
 
     private fun buildImage() {
-
+        imageViewMascot = findViewById(R.id.mascot)
+        imageViewMascot.visibility = View.VISIBLE
     }
 }
