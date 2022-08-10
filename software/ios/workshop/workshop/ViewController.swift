1bb59848db7a9e0cc97f1aa95ed57535967d946d
Nu'man Naufal <numan.naufal@gmail.com>
2022-08-10 12:06:42 +0700

    Software > iOS > initial project

---
+++ software/ios/workshop/workshop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  workshop
+//
+//  Created by Nu'man on 10/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
