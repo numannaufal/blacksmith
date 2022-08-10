6529198cf88b36cac0e95e3ef54142ff21fb007e
Nu'man Naufal <numan.naufal@gmail.com>
2022-08-10 16:46:48 +0700

    Software > iOS > Add image

--- software/ios/workshop/workshop/ViewController.swift
+++ software/ios/workshop/workshop/ViewController.swift
@@ -8,11 +8,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var title_label: UILabel!
+    @IBOutlet var mascot: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        titleLabel.text = "Blacksmith"
+        title_label.text = "Blacksmith"
+        mascot.image = UIImage(named: "Mascot")
         // Do any additional setup after loading the view.
     }
 
