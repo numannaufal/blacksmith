36504f17130aa51827ac65adfc92e8eb6a9e27cb
Nu'man Naufal <numan.naufal@gmail.com>
2022-08-20 11:59:23 +0700

    Software > iOS > Wrap up goto function

--- software/ios/forgery/forgery/HomeController.swift
+++ software/ios/forgery/forgery/HomeController.swift
@@ -17,25 +17,8 @@ class HomeController: UIViewController {
     }
     
     @IBAction func clickNextButton(_ sender: UIButton) {
-        print("clicked MAN!")
-        self.goToForgery()
+        let navController = self.navigationController as! NavigationController
+        navController.goToForgery()
     }
     
-    func goToForgery() {
-        let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-        let forgeryController = storyBoard.instantiateViewController(withIdentifier: "ForgeryController") as! ForgeryController
-        self.navigationController?.pushViewController(forgeryController, animated: true)
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- software/ios/forgery/forgery/NavigationController.swift
+++ software/ios/forgery/forgery/NavigationController.swift
@@ -8,19 +8,33 @@
 import UIKit
 
 class NavigationController: UINavigationController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("navigation controller")
         DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
             self.goToHome()
         }
     }
     
     func goToHome() {
+        self.goTo(id: "HomeController", Controller: HomeController())
+    }
+    
+    func goToForgery() {
+        self.goTo(id: "ForgeryController", Controller: ForgeryController())
+    }
+    
+    func goTo<T>(id: String, Controller: T) {
+        let aViewController = self.getController(
+            controllerIdentifier: id,
+            Controller: T.self
+        )
+        self.pushViewController(aViewController, animated: true)
+    }
+    
+    func getController<T>(controllerIdentifier: String, Controller: T) -> UIViewController {
         let storyBoard = UIStoryboard(name: "Main", bundle: nil)
-        let homeController = storyBoard.instantiateViewController(withIdentifier: "HomeController") as! HomeController
-        self.pushViewController(homeController, animated: true)
+        return storyBoard.instantiateViewController(withIdentifier: controllerIdentifier)
     }
 }
 
